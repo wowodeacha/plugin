9936e516e865ac22b0cfcd0667b4be0cbe43603f
wowodeacha <wowodeacha@gmail.com>
2016-09-19 13:41:24 +0800

    just commit

--- plugin_rig/facerigmodule/FaceRigUISetUp.py
+++ plugin_rig/facerigmodule/FaceRigUISetUp.py
@@ -8,7 +8,7 @@
 from plugin_rig.ui.facerig import Ui_faceUI_Form
 from PyQt4 import QtGui,QtCore
 import maya.cmds as mpy
-from customAttrSet import customAttrSetCla
+from plugin_rig.customAttrSet import customAttrSetCla
 
 
 #设置表情窗口
