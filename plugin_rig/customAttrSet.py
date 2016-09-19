66f5630538c0511028306d3b1bbc8eb2b7abaebd
wowodeacha <wowodeacha@gmail.com>
2016-09-19 15:38:45 +0800

    just commit

--- plugin_rig/customAttrSet.py
+++ plugin_rig/customAttrSet.py
@@ -6,15 +6,17 @@ date 2016/08/26
 '''
 import os,inspect
 
+
 class CustomAttrSetCla(object):
     def __init__(self):
         pass
 
-    # current fuction dir 获取当前脚本路径
-    def getCurDirFun(self):
-        curFile = inspect.getfile(inspect.currentframe())
-        Curpath = os.path.dirname(curFile) + "/"
-        return Curpath
+    # current function dir 获取当前脚本路径
+    @staticmethod
+    def get_cur_dir_fun(self):
+        cur_file = inspect.getfile(inspect.currentframe())
+        cur_path = os.path.dirname(cur_file) + "/"
+        return cur_path
 
 if __name__ == '__main__':
     CSet = CustomAttrSetCla()
