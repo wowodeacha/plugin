0cd87eeed823990af8137416454faae835978ae1
wowodeacha <wowodeacha@gmail.com>
2016-09-19 16:07:33 +0800

    add install py

--- install.py
+++ install.py
@@ -2,22 +2,23 @@
 #
 # author yangjie
 # mail wowodeacha@gmail.com
-# date 2016/08/26
+# date 2016/09/19
 #
 import os,inspect
 
 
-class CustomAttrSetCla(object):
+class IntallPlug(object):
     def __init__(self):
         pass
 
     # current function dir 获取当前脚本路径
     @staticmethod
-    def get_cur_dir_fun(self):
+    def get_cur_dir_fun():
         cur_file = inspect.getfile(inspect.currentframe())
         cur_path = os.path.dirname(cur_file) + "/"
+        print cur_path
         return cur_path
 
 if __name__ == '__main__':
-    CSet = CustomAttrSetCla()
-    CSet.getCurDirFun()
+    CSet = IntallPlug()
+    CSet.get_cur_dir_fun()
