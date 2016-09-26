483e5deadfb66f01dbe408f4a8957ecfb702349a
wowodeacha <wowodeacha@gmail.com>
2016-09-26 16:47:45 +0800

    head jnt create test

--- plugin_rig/printText.py
+++ plugin_rig/printText.py
@@ -1,11 +1,6 @@
-#Created by yang jie
-import json
-
-
-class PrintText():
-    def __init__(self):
-        pass
-    def printTex(self):
-        print "Hi Peng You"
-
-
+import custom_global_function as cgf
+CAS = cgf.CustomAttrSetCla()
+PLUGIN_PATH = CAS.get_cur_dir_path_fun()
+DATA_PATH = PLUGIN_PATH + "datafile/headbasebonedata.json"
+FACE_JNT_PIV_DIR = CAS.load_data(DATA_PATH)
+CAS.write_data(DATA_PATH,FACE_JNT_PIV_DIR)
