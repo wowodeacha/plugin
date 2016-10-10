9cd89f48e9a2a9a5abb3340283d866e427b4f02a
yangjie <wowodeacha@gmail.com>
2016-10-10 10:11:42 +0800

    del

--- plugin_rig/printText.py
+++ plugin_rig/printText.py
@@ -1,6 +1,1 @@
-import custom_global_function as cgf
-CAS = cgf.CustomAttrSetCla()
-PLUGIN_PATH = CAS.get_cur_dir_path_fun()
-DATA_PATH = PLUGIN_PATH + "datafile/headbasebonedata.json"
-FACE_JNT_PIV_DIR = CAS.load_data(DATA_PATH)
-CAS.write_data(DATA_PATH,FACE_JNT_PIV_DIR)
+print "hello"
